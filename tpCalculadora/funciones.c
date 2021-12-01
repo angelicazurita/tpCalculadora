73eaae6e992614819c285f2254a2e26ac5bd25ca
MicaelaGomez09 <92445801+MicaelaGomez09@users.noreply.github.com>
2021-12-01 03:36:13 -0300

    FINAL

--- tpCalculadora/funciones.c
+++ tpCalculadora/funciones.c
@@ -1,15 +1,19 @@
 
 void mostrarMenu()
 {
-    printf("\n \n   MENU DE OPCIONES: \n");
-    printf("\n   1-SUMA \n");
-    printf("   2-RESTA \n");
-    printf("   3-DIVISION \n");
-    printf("   4-MULTIPLICACION \n");
-    printf("   5-FACTORIAL \n");
-    printf("   6-TODAS LAS OPERACIONES \n");
-    printf("   7-SALIR \n");
-    printf("\n ELEGIR NUMERO DE OPERACION:");
+    printf("\n                                           +---------------------------+\n");
+    printf("                                           ¦      Menu de opciones     ¦\n");
+    printf("                                           +---------------------------+\n");
+    printf("                                           -----------------------------\n");
+    printf("                                           ¦  1-SUMA                   ¦\n");
+    printf("                                           ¦  2-RESTA                  ¦\n");
+    printf("                                           ¦  3-DIVISION               ¦\n");
+    printf("                                           ¦  4-MULTIPLICACION         ¦\n");
+    printf("                                           ¦  5-FACTORIAL              ¦\n");
+    printf("                                           ¦  6-TODAS LAS OPERACIONES  ¦\n");
+    printf("                                           ¦  7-SALIR                  ¦\n");
+    printf("                                           -----------------------------\n");
+    printf("                                           ELEGIR NUMERO DE OPERACION: ");
 
 }
 
@@ -36,7 +40,8 @@ float calculaDIVISION(float A,float  B)
 
     if(B == 0)
     {
-        printf("No es posible la division de un numero por cero \n ");
+        printf("\n                                           No es posible la division de\n");
+        printf("                                                un numero por cero \n");
     }
     else
     {
@@ -57,11 +62,14 @@ float calculaMULTIPLICACION(float A,float  B)
 //5 Calcular el factorial
 int calculaFACTORIAL(int A)
 {
-    int factorial = 1;
+    int factorial;
 
-    for(int i = A; i > 0; i--)
-    {
-        factorial = factorial * i;
+    if(A <1 ){
+        return 0;
+    }else if(A == 1){
+    return 1;
+    }else{
+    return A*calculaFACTORIAL(A-1);
     }
     return factorial;
 }
@@ -82,24 +90,31 @@ void calculaYmuestraTODAS(float A,float B)
     else
     {
         resultado = calculaSUMA( A, B);
-        printf("\n SUMA: %.1f + %.1f = %.1f",A,B, resultado);
+        printf("\n                                           SUMA: %.1f + %.1f = %.1f",A,B, resultado);
 
         resultado= calculaRESTA( A,  B);
-        printf("\n RESTA: %.1f - %.1f = %.1f",A,B, resultado);
+        printf("\n                                           RESTA: %.1f - %.1f = %.1f",A,B, resultado);
 
         resultado= calculaMULTIPLICACION( A, B);
-        printf("\n MULTIPLICACION: %.1f x %.1f = %.1f",A,B, resultado);
+        printf("\n                                           MULTIPLICACION: %.1f x %.1f = %.1f",A,B, resultado);
 
         resultado= A/B;
-        printf("\n DIVISION: %.1f / %.1f = %.1f",A, B, resultado);
-
-        //printf("\n Ingrese un numero para factorial: \n");
-        //scanf("%d", &A);
+        printf("\n                                           DIVISION: %.1f / %.1f = %.1f",A, B, resultado);
 
         resultado= calculaFACTORIAL (A);
         // printf("\n FACTORIAL %d= %d", A, resultado);
-        printf("\n El factorial de %.0f es: %.0f ",A,resultado);
-
+        printf("\n                                           FACTORIAL de %.0f es: %.0f ",A,resultado);
+        printf("\n\n");
         return;
     }
+}
+
+
+int validaRango(int A ){
+  if ( A >=0 && A <= 31){
+    return 1;
+  }else{
+ return 0;
+  }
+
 }
